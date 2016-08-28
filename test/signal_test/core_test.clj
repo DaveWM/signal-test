f597c8ac932df56b97f2e63b1e65bcb49724a931
David Martin <davidm@potatolondon.com>
2016-08-28 15:58:10 +0100

    added basic tests

--- test/signal_test/core_test.clj
+++ test/signal_test/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [signal-test.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest number-to-string-tests
+  (is (= (number-to-string 1) "one"))
+  (is (= (number-to-string 123) "one hundred and twenty three"))
+  (is (= (number-to-string 12) "twelve"))
+  (is (= (number-to-string 44) "forty four"))
+  (is (= (number-to-string 1234) "one thousand, two hundred and thirty four"))
+  (is (= (number-to-string 1001) "one thousand, and one"))
+  (is (= (number-to-string 1001001) "one million, one thousand, and one")))
